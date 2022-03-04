2d4b0165a87cf17ca95ecd5e0d1533cb007b15ae
rezasofyan <ythnobi@gmail.com>
2022-03-04 15:08:08 +0700

    Menambahkan file switch case dan ternary.js

---
+++ conditional/ternary.js
@@ -0,0 +1,4 @@
+var age= 20
+var bisaVote= age > 20 ? "bisa vote" : "belum bisa vote" // hasilnya belum bisa vote
+var angka = 9
+var jenisBilangan = angka % 2 === 0 ? "Bilangan Genap" : "Bukan Bilangan Genap" //hasilnya bukan bilangan genap
