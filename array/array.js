6f718e7573745edaa9aef67da737bd976171e9d2
rezasofyan <ythnobi@gmail.com>
2022-03-08 11:38:44 +0700

    Menambahkan file array.js dan array.txt

---
+++ array/array.js
@@ -0,0 +1,18 @@
+
+//contoh array sederhana
+var array = [2, 5, 1, 3, 4]
+
+console.log( array[0] ) /// outputnya -> 2
+console.log( array[2] ) /// outputnya -> 1
+
+console.log("===================================")
+
+//.length 
+var hobbies = ["coding", "cycling", "climbing", "skateboarding"] 
+console.log(hobbies) // [ 'coding', 'cycling', 'climbing', 'skateboarding' ]
+console.log(hobbies.length) // 4 
+console.log(hobbies[0]) // coding
+console.log(hobbies[2]) // climbing
+// Mengakses elemen terakhir dari array
+console.log(hobbies[hobbies.length -1]) // skateboarding
+
